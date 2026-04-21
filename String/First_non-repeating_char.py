4a8982cc664c427604e6a11c84bcb664ee24fe88
Rakhi choudhary <146644990+rkrakhikumari@users.noreply.github.com>
2026-04-21 12:08:02 +0530

    Create first_non_repeating_char.py

---
+++ String/First_non-repeating_char.py
@@ -0,0 +1,10 @@
+def first_non_repeating_char(s):
+  freq = {}
+  for ch in s:
+    freq[ch] = freq.get(ch, 0)+ 1
+  for ch in s:
+    if freq[ch] == 1:
+      return ch
+  return None
+
+print(first_non_repeating_char("Choudhary"))
